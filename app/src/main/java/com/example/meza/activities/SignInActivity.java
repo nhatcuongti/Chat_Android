b9f8ffad2f7adcc07deb155fa39639ecf39d4f0a
Cpp4Life <Cpp4Life@users.github.com>
2022-03-03 20:48:51 +0700

    Handle events on sign in screen v1.0

--- app/src/main/java/com/example/meza/activities/SignInActivity.java
+++ app/src/main/java/com/example/meza/activities/SignInActivity.java
@@ -2,7 +2,11 @@ package com.example.meza.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.meza.databinding.ActivitySignInBinding;
 
@@ -15,5 +19,49 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(v -> {
+            if (isValidSignInDetails())
+                signUp();
+        });
+    }
+
+    private void signUp() {
+        loading(true);
+        showToast("Fetching to login in");
+    }
+
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSignInDetails() {
+        if (binding.inputPhone.getText().toString().trim().isEmpty()) {
+            showToast("Nhập số điện thoại");
+            return false;
+        } else if (!Patterns.PHONE.matcher(binding.inputPhone.getText().toString()).matches()) {
+            showToast("Nhập số điện thoại hợp lệ");
+            return false;
+        } else if (binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Nhập mật khẩu");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
