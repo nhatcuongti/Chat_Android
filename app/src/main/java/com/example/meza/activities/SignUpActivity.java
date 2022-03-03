ae97813b5dcd3e0ffea9559d9bf51d52533171c9
Cpp4Life <Cpp4Life@users.github.com>
2022-03-03 21:14:17 +0700

    handle events in sign up activity v1.0

--- app/src/main/java/com/example/meza/activities/SignUpActivity.java
+++ app/src/main/java/com/example/meza/activities/SignUpActivity.java
@@ -3,6 +3,9 @@ package com.example.meza.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.meza.databinding.ActivitySignUpBinding;
 
@@ -15,5 +18,57 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+        binding.buttonSignUp.setOnClickListener(v -> {
+            if(isValidSignUpDetails())
+                signUp();
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signUp() {
+        loading(true);
+        showToast("Signing you up");
+    }
+
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private Boolean isValidSignUpDetails() {
+        if (binding.inputUsername.getText().toString().trim().isEmpty()) {
+            showToast("Nhập tên người dùng");
+            return false;
+        } else if (binding.inputPhone.getText().toString().trim().isEmpty()) {
+            showToast("Nhập số điện thoại");
+            return false;
+        } else if (!Patterns.PHONE.matcher(binding.inputPhone.getText().toString()).matches()) {
+            showToast("Nhập số điện thoại hợp lệ");
+            return false;
+        } else if (binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Nhập mật khẩu");
+            return false;
+        } else if (binding.inputConfirmPassword.getText().toString().trim().isEmpty()) {
+            showToast("Nhập lại mật khẩu");
+            return false;
+        } else if (!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())) {
+            showToast("Mật khẩu không trùng nhau");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
