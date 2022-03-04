4e88cca6451b4f456a754586e713a6a3bf904e1d
nhatcuongti <nhatcuongti@gmail.com>
2022-03-04 16:25:34 +0700

    Finish UI of Chat

---
+++ app/src/main/java/com/example/meza/activities/ChatActivity.java
@@ -0,0 +1,19 @@
+package com.example.meza.activities;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.meza.R;
+
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+}
